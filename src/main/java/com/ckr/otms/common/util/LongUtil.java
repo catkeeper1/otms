e5c6334e2191a3e1966343d1f084e20c14bd9d85
catkeeper1 <catkeeper1@gmail.com>
2017-02-02 00:33:09 +0800

    fix checkstyle warning

--- src/main/java/com/ckr/otms/common/util/LongUtil.java
+++ src/main/java/com/ckr/otms/common/util/LongUtil.java
@@ -1,13 +1,13 @@
 package com.ckr.otms.common.util;
 
 public class LongUtil {
-	public static Long parse(String str){
-		
-		try{
-			return Long.valueOf(str);
-		}catch(NumberFormatException e){
-			return null;
-		}
-		
-	}
+    public static Long parse(String str) {
+
+        try {
+            return Long.valueOf(str);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+
+    }
 }
